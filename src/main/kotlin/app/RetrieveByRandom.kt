d5ac2558e6b25267bc365192a1b9142227d422f9
hafidjs <afidzpratama@gmail.com>
2024-10-19 11:06:13 +0700

    Retrieve By Random

---
+++ src/main/kotlin/app/RetrieveByRandom.kt
@@ -0,0 +1,12 @@
+package app
+
+fun main() {
+    val numbers = (1..10).toList()
+
+    println(numbers.random())
+    println(numbers.random())
+    println(numbers.random())
+    println(numbers.random())
+    println(numbers.random())
+
+}
